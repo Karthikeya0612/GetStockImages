224934836ad54a4b5d2829359698cfcf3960b7f5
Uppu Karthikeya <97943322+Karthikeya0612@users.noreply.github.com>
2024-05-13 23:16:38 +0530

    Displaying the unstructured data from unsplash

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 import Header from './components/Header'
 import Form from './components/Form'
+import axios from 'axios';
+
+const API_URL = 'https://api.unsplash.com/search/photos';
 function App() {
-  const [searchQuery, setSearchQuery] = useState('');
+
+  const [images, setImages] = useState([]);
+  const [totalImages, setTotalImages] = useState(0);
+
+  const fetchImages = async (query) => {
+    try {
+      const response = await axios.get(`${API_URL}?query=${query}&page=1&per_page=20&client_id=${import.meta.env.VITE_API_KEY}`);
+      setImages(response.data.results);
+      setTotalImages(response.data.total_pages);
+      // console.log(images);
+      // console.log(totalImages);
+
+    }
+    catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleFormSubmit = (query) => {
-    setSearchQuery(query);
     console.log(query);
     console.log(import.meta.env.VITE_API_KEY);
     // You can perform further actions here, such as fetching images based on the query
+    fetchImages(query);
   };
   return (
     <>
       <Header/> 
-      <Form onSubmit={handleFormSubmit}/>   
+      <Form onSubmit={handleFormSubmit}/> 
+      <div className="container">
+        <div className="row">
+          {images.map((image) => (
+            <div key={image.id} className="col-md-4">
+              <img src={image.urls.small} alt={image.alt_description} className="img-fluid"/>
+            </div>
+          ))}
+        </div>  
+      </div>
     </>
   )
 }
